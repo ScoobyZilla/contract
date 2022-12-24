813ff261c8c7d8a13a46d35ee33e889c47693e38
ScoobyZilla <121004361+ScoobyZilla@users.noreply.github.com>
2022-12-24 00:43:00 -0300

    Add files via upload

---
+++ constructorArguments.ts
@@ -0,0 +1,9 @@
+const constructorArguments: [string, string, number, string, string] = [
+  "ScoobyZilla",
+  "SBZ",
+  18,
+  "0xA228F4Aa89C583d50e5d1db445Dd674C9F228D07", // marketing wallet
+  "0x10ED43C718714eb63d5aA57B78B54704E256024E", // router address
+];
+
+export default constructorArguments;
